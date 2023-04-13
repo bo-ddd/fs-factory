47a835334239d421c72607c0c40776e1c7501e84
F-17-2006 <3375926016@qq.com>
2023-04-13 14:34:35 +0800

    推

---
+++ src/components/EnergyManagementView.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="title-box">
+    <div class="heteromorphic-box">
+      <div class="heteromorphic-b"></div>
+      <div class="icon-box">
+        <img class="arrowhead" src="@/assets/images/arrowhead.png" alt="" />
+        <div class="heteromorphic-a">
+          <div class="title-size">
+            {{ title }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="body-box">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+body {
+  .title-box {
+    height: 100%;
+    margin: 0 auto;
+    border-radius: 0.5rem;
+    .heteromorphic-box {
+      height: 20%;
+      .icon-box {
+        display: flex;
+        .arrowhead {
+          width: 1.5rem;
+          height: 1.5rem;
+          position: relative;
+          top: -0.6rem;
+        }
+      }
+      .heteromorphic-b {
+        border-bottom: 0.1rem solid;
+        margin-left: 19.6rem;
+        border-image: linear-gradient(220deg, rgb(0, 89, 255, 0.2) 0%, #ffffff 80%) 2 2 2 2;
+      }
+      .heteromorphic-a {
+        width: 17rem;
+        height: 3.2rem;
+        display: flex;
+        align-items: center;
+        transform: skew(-33deg);
+        border-bottom: 0.1rem solid;
+        border-right: 0.1rem solid;
+        border-image: linear-gradient(100deg, rgb(0, 89, 255, 0.2) 0%, #ffffff 80%) 2 2 2 2;
+        background-image: linear-gradient(165deg, rgba(0, 0, 0, 0), rgba(0, 21, 255, 0.3));
+        .title-size {
+          // margin-bottom: 3.5rem;
+          // font-size: 2.5rem;
+          margin-bottom: 3rem;
+          font-size: 1.8rem;
+          font-weight: 700;
+          color: #fff;
+          transform: skew(20deg);
+        }
+      }
+    }
+    .body-box{
+      height: 80%;
+    }
+  }
+}
+</style>
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+let props = defineProps<{
+  title?: string
+}>()
+const { title } = props
+</script>
