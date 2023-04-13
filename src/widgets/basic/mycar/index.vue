8244c8680624c051dacbaffcdde60fe98386b1ab
yang <1145938287@qq.com>
2023-04-13 15:19:39 +0800

    css

---
+++ src/widgets/basic/mycar/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { onUnmounted } from "vue"
+import useLifecycle from "@mars/common/uses/use-lifecycle"
+import * as mapWork from "./map"
+
+// 启用map.ts生命周期
+useLifecycle(mapWork)
+console.log('aaaaa');
+
+onUnmounted(() => {
+  // 销毁操作
+})
+</script>
+<style lang="less">
+</style>
