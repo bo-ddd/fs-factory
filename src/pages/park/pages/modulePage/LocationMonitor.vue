ea025ff63dcbec14b25c822b50ed2962fa18b429
yang <1145938287@qq.com>
2023-04-13 10:53:22 +0800

    css

--- src/pages/park/pages/modulePage/LocationMonitor.vue
+++ src/pages/park/pages/modulePage/LocationMonitor.vue
@@ -1,5 +1,17 @@
 <template>
-<div>
-        page
-</div>
+  <div>
+    <div class="a">
+      a
+      <div class="b">b</div>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="less">
+.a {
+  color: red;
+  .b {
+    color: green;
+  }
+}
+</style>
